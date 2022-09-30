0a350780f4f791c713efe63414a8dfecf22a97f5
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2022-09-30 14:58:56 +0200

    first commit
    Co-Authored-By: Kiwi <59049734+WillyTrek19@users.noreply.github.com>
    Co-Authored-By: WittIsHere <59049883+WittIsHere@users.noreply.github.com>

---
+++ Assets/_Scripts/EventHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+	public PlayerClass pcBuffer;
+	public List<PlayerClass> ListPC;
+	public string url = "https://citmalumnes.upc.es/~sergicf4/";
+	public string phpUrl = "AddPlayer.php";
+	public string dataUrl = "";
+
+	private void OnEnable()
+	{
+		Simulator.OnNewPlayer += NewPlayer;
+	}
+
+	private void NewPlayer(string arg1, string arg2, DateTime arg3)
+	{
+		pcBuffer.name = arg1;
+		pcBuffer.country = arg2;
+		pcBuffer.dateTime = arg3;
+		ListPC.Add(pcBuffer);
+		Player2PHP(pcBuffer);
+	}
+	IEnumerator Player2PHP(PlayerClass pc)
+	{
+		dataUrl = url + phpUrl + pc.GetData();
+
+		using (WWW www = new WWW(dataUrl))
+		{
+			yield return www;
+		}
+	}
+}
---
+++ Assets/_Scripts/PlayerClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClass
+{
+	public string name, country;
+	public DateTime dateTime;
+	public PlayerClass(string name, string country, DateTime dateTime)
+	{
+		this.name = name;
+		this.country = country;
+		this.dateTime = dateTime;
+	}
+	public string GetData()
+	{
+		string data = "name=" + name + "+country=" + country + "+dateTime=" + dateTime.ToString();
+		return data;
+	}
+
+}
