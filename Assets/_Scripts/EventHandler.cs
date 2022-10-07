30b4284aeb7e61da97f109181d1109761456432a
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2022-10-07 15:06:32 +0200

    sessions started & players done
    Co-Authored-By: Kiwi <59049734+WillyTrek19@users.noreply.github.com>

--- Assets/_Scripts/EventHandler.cs
+++ Assets/_Scripts/EventHandler.cs
@@ -6,33 +6,77 @@ using UnityEngine;
 public class EventHandler : MonoBehaviour
 {
 	public PlayerClass pcBuffer;
-	public List<PlayerClass> ListPC;
+	public uint pcId;
+	//public List<PlayerClass> ListPC;
+	public SessionClass sBuffer;
+	public uint sId;
+
 	public string url = "https://citmalumnes.upc.es/~sergicf4/";
-	public string phpUrl = "AddPlayer.php";
+	public string pcUrl = "AddPlayer.php";
+	public string sUrl = "AddSession.php";
 	public string dataUrl = "";
 
 	private void OnEnable()
 	{
 		Simulator.OnNewPlayer += NewPlayer;
+		Simulator.OnNewSession += NewSession;
 	}
 
-	private void NewPlayer(string arg1, string arg2, DateTime arg3)
+	private void NewPlayer(string name, string country, DateTime dateTime)
 	{
-		pcBuffer = new PlayerClass(arg1, arg2, arg3);
+		pcBuffer = new PlayerClass(name, country, dateTime);
 		//ListPC.Add(pcBuffer);
 		StartCoroutine(Player2PHP(pcBuffer));
 	}
 	IEnumerator Player2PHP(PlayerClass pc)
 	{
-		dataUrl = url + phpUrl + "?" + pc.GetData();
+		dataUrl = url + pcUrl + "?" + pc.GetData();
 		Debug.Log(dataUrl);
-		using (WWW www = new WWW(dataUrl))
-		{
-			yield return www;
+		WWW www = new WWW(dataUrl);
+		
+		yield return www;
 
-			//TODO: Save UID in Unity
-			//Simulator.OnAddPlayerCallback
-			//www.text
+		if(www.error == null)
+		{
+			Debug.Log(www.text);
+			pcId = uint.Parse(www.text);
+			CallbackEvents.OnAddPlayerCallback?.Invoke(pcId);
 		}
+		else
+		{
+			Debug.Log(www.error);
+		}
+		//TODO: Save UID in Unity
+		//Simulator.OnAddPlayerCallback
+		//www.text
+	}
+
+	private void NewSession(DateTime dateTime)
+	{
+		sBuffer = new SessionClass(dateTime, pcId);
+		StartCoroutine(SessionStart2PHP(sBuffer));
+	}
+
+	IEnumerator SessionStart2PHP(SessionClass s)
+	{
+		dataUrl = url + sUrl + "?" + "type=start&" + s.GetData();
+		Debug.Log(dataUrl);
+		WWW www = new WWW(dataUrl);
+
+		yield return www;
+
+		if (www.error == null)
+		{
+			Debug.Log(www.text);
+			//sId = uint.Parse(www.text);
+			//CallbackEvents.OnNewSessionCallback?.Invoke(sId);
+		}
+		else
+		{
+			Debug.Log(www.error);
+		}
+		//TODO: Save UID in Unity
+		//Simulator.OnAddPlayerCallback
+		//www.text
 	}
 }
---
+++ Assets/_Scripts/SessionClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionClass : MonoBehaviour
+{
+    public DateTime dateTime;
+    public uint id;
+
+    public SessionClass(DateTime dateTime, uint id)
+    {
+        this.dateTime = dateTime;
+        this.id = id;
+    }
+
+    public string GetData()
+    {
+        string data = "dateTime=" + dateTime.ToString("yyyy-MM-dd HH:mm:ss") + "&id=" + id;
+        return data;
+    }
+}
