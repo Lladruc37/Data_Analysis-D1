31b01f12b03ba96208e80eef8624e54d64003be7
Kiwi <59049734+WillyTrek19@users.noreply.github.com>
2022-10-14 14:53:43 +0200

    Recording purchases & session ends
    Co-Authored-By: Lladruc <59049906+Lladruc37@users.noreply.github.com>

--- Assets/_Scripts/EventHandler.cs
+++ Assets/_Scripts/EventHandler.cs
@@ -9,20 +9,26 @@ public class EventHandler : MonoBehaviour
 	public uint pcId;
 	//public List<PlayerClass> ListPC;
 	public SessionClass sBuffer;
+	public ItemClass iBuffer;
 	public uint sId;
+	public uint eId;
 
 	public string url = "https://citmalumnes.upc.es/~sergicf4/";
 	public string pcUrl = "AddPlayer.php";
 	public string sUrl = "AddSession.php";
+	public string bUrl = "AddBuy.php";
+	public string eUrl = "EndSession.php";
 	public string dataUrl = "";
 
 	private void OnEnable()
 	{
 		Simulator.OnNewPlayer += NewPlayer;
 		Simulator.OnNewSession += NewSession;
+		Simulator.OnBuyItem += NewPurchase;
+		Simulator.OnEndSession += EndSession;
 	}
 
-	private void NewPlayer(string name, string country, DateTime dateTime)
+    private void NewPlayer(string name, string country, DateTime dateTime)
 	{
 		pcBuffer = new PlayerClass(name, country, dateTime);
 		//ListPC.Add(pcBuffer);
@@ -59,7 +65,7 @@ public class EventHandler : MonoBehaviour
 
 	IEnumerator SessionStart2PHP(SessionClass s)
 	{
-		dataUrl = url + sUrl + "?" + "type=start&" + s.GetData();
+		dataUrl = url + sUrl + "?" + s.GetData();
 		Debug.Log(dataUrl);
 		WWW www = new WWW(dataUrl);
 
@@ -68,13 +74,69 @@ public class EventHandler : MonoBehaviour
 		if (www.error == null)
 		{
 			Debug.Log(www.text);
-			//sId = uint.Parse(www.text);
-			//CallbackEvents.OnNewSessionCallback?.Invoke(sId);
+            sId = uint.Parse(www.text);
+            CallbackEvents.OnNewSessionCallback?.Invoke(sId);
+        }
+		else
+		{
+			Debug.Log(www.error);
+		}
+		//TODO: Save UID in Unity
+		//Simulator.OnAddPlayerCallback
+		//www.text
+	}
+
+	private void NewPurchase(int itemId, DateTime buyTime)
+	{
+		iBuffer = new ItemClass(itemId, buyTime);
+		StartCoroutine(Item2PHP(iBuffer));
+	}
+
+	IEnumerator Item2PHP(ItemClass it)
+	{
+		bUrl = "AddBuy.php";
+		dataUrl = url + bUrl + "?playerId=" + pcId + "&" + it.GetData();
+		Debug.Log(dataUrl);
+		WWW www = new WWW(dataUrl);
+
+		yield return www;
+
+		if (www.error == null)
+		{
+			Debug.Log(www.text);
+			CallbackEvents.OnItemBuyCallback?.Invoke();
 		}
 		else
 		{
 			Debug.Log(www.error);
 		}
+	}
+
+	private void EndSession(DateTime dateTime)
+	{
+		sBuffer = new SessionClass(dateTime, sId);
+		StartCoroutine(SessionEnd2PHP(sBuffer));
+	}
+
+	IEnumerator SessionEnd2PHP(SessionClass s)
+	{
+		dataUrl = url + eUrl + "?" + s.GetData();
+		Debug.Log(dataUrl);
+		WWW www = new WWW(dataUrl);
+
+		yield return www;
+
+		if (www.error == null)
+		{
+			Debug.Log(www.text);
+            eId = uint.Parse(www.text);
+            CallbackEvents.OnEndSessionCallback?.Invoke(eId);
+        }
+		else
+		{
+			Debug.Log(www.error);
+		}
+
 		//TODO: Save UID in Unity
 		//Simulator.OnAddPlayerCallback
 		//www.text
---
+++ Assets/_Scripts/ItemClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClass
+{
+	public int itemId;
+	public DateTime buyTime;
+	public ItemClass(int id, DateTime buyTime)
+	{
+		this.itemId = id;
+		this.buyTime = buyTime;
+	}
+	public string GetData()
+	{
+		string data = "itemId=" + itemId + "&buyTime=" + buyTime.ToString("yyyy-MM-dd HH:mm:ss");
+		return data;
+	}
+
+}
